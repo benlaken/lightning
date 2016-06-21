d87464445166bf0a674f468846b09d6909a20528
Benjamin A. Laken <benlaken@gmail.com>
2016-06-21 17:00:43 +0100

    added test data

--- stormstats/tests/test_storm.py
+++ stormstats/tests/test_storm.py
@@ -1,6 +1,7 @@
 import os
 import sys
-from ..storm import Storm
+import pandas as pd
+from ..storm import Storm, read_WWLN
 
 
 def test_reading_function():
@@ -8,3 +9,11 @@ def test_reading_function():
     is a function. """
     def foo(): pass
     assert type(Storm.read_data) == type(foo)
+
+
+def test_examine_date_structure():
+    test_data = read_WWLN("egdata/testdata.loc")
+    """Check datetime is right type and elements of the datetime are ints"""
+    assert isinstance(test_data['datetime'][0], pd.tslib.Timestamp)
+    assert isinstance(test_data['datetime'][0].date().year, int)
+    assert isinstance(test_data['datetime'][0].time().microsecond, int)
