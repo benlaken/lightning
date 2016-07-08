f26db6d994613106364b1334344ba4ca8985c130
Benjamin A. Laken <benlaken@gmail.com>
2016-07-08 11:29:23 +0100

    tests passing in 2.7 and 3.5 now

--- stormstats/tests/test_storm.py
+++ stormstats/tests/test_storm.py
@@ -4,16 +4,10 @@ import pandas as pd
 from ..storm import Storm, read_WWLN
 
 
-def test_reading_function():
-    """Example of how to run a test. In this case to see if reading
-    is a function. """
-    def foo(): pass
-    assert type(Storm.read_data) == type(foo)
-
 
 def test_examine_date_structure():
-    test_data = read_WWLN("stormstats/egdata/testdata.loc")
     """Check datetime is right type and elements of the datetime are ints"""
+    test_data = read_WWLN("stormstats/egdata/testdata.loc")
     assert isinstance(test_data['datetime'][0], pd.tslib.Timestamp)
     assert isinstance(test_data['datetime'][0].date().year, int)
     assert isinstance(test_data['datetime'][0].time().microsecond, int)
